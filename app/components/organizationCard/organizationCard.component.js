8ae7cff59efe8dd8a08d7cc681fd7ba9d865a7c3
Justin Palmer <Justin.Palmer@walmart.com>
2016-10-22 17:28:13 -0500

    Updated org card component styling.

--- app/components/organizationCard/organizationCard.component.js
+++ app/components/organizationCard/organizationCard.component.js
@@ -3,8 +3,7 @@
         .module('app')
         .component('organizationCard', {
             bindings: {
-                icon: '@',
-                iconText: '@'
+                logo: '@',
             },
             templateUrl: 'app/components/organizationCard/organization-card.tpl.html',
             controller: OrganizationCardController,
@@ -13,6 +12,11 @@
 
     function OrganizationCardController() {
         var vm = this;
+
+        vm.orgName = "Care Community Center";
+        vm.orgAddress = "2510 N 17th St #203Rogers, AR 72756";
+        vm.orgPhone = "(479) 246-0104";
+        vm.orgHours = "Open today · 9AM–4PM";
 
         vm.$onInit = function() {
 
