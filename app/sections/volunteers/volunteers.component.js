88b4c1b66cdc2fe5bb4b3fe2ccc91f6716260009
Justin Palmer <Justin.Palmer@walmart.com>
2016-11-07 14:46:47 -0600

    Modified edit volunteer template.

--- app/sections/volunteers/volunteers.component.js
+++ app/sections/volunteers/volunteers.component.js
@@ -104,10 +104,13 @@
         };
 
 
-        NewVolunteerController.$inject = ['$mdDialog'];
+        NewVolunteerController.$inject = ['$mdDialog', 'userService'];
 
-        function NewVolunteerController($mdDialog) {
+        function NewVolunteerController($mdDialog, userService) {
             let vm = this;
+
+            vm.organizations = userService.getOrganizations();
+            vm.selectOrgs = vm.organizations.map(function (org) { return { value: org.name }; });
 
             vm.dismiss = function () {
                 $mdDialog.hide();
