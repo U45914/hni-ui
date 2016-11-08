c5d13ed8f8f76aeae24905eec98d7ce93829afc3
James Glenn <jglenn1@gmail.com>
2016-11-08 17:14:22 -0600

    images and stuff

--- app/sections/landing/landingNgo/landingNgo.component.js
+++ app/sections/landing/landingNgo/landingNgo.component.js
@@ -10,5 +10,36 @@
 
     function LandingNgoController() {
         var vm = this;
+
+        vm.ngos = [
+            {
+                name: "NWA Women's Shelter",
+                imagePath: "/app/assets/images/landing/nwa_womens_shelter.png"
+            },
+            {
+                name: "Samaritan Community Center",
+                imagePath: "/Content/"
+            },
+            {
+                name: "Teen Action & Support Center",
+                imagePath: "/Content/"
+            },
+            {
+                name: "7hills",
+                imagePath: "/Content/"
+            },
+            {
+                name: "Care Community Center",
+                imagePath: "/Content/"
+            },
+            {
+                name: "Genesis House",
+                imagePath: "/Content/"
+            },
+            {
+                name: "The Manna Center",
+                imagePath: "/Content/"
+            },
+        ];
     }
 })();
