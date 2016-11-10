369f5d99afa10bc501447b4ce79bc45cb9480bfd
James Glenn <jglenn1@gmail.com>
2016-11-10 09:20:21 -0600

    Adding in the donation tracker/form code

--- app/sections/landing/landingDonate/landingDonate.component.js
+++ app/sections/landing/landingDonate/landingDonate.component.js
@@ -10,5 +10,13 @@
 
     function LandingDonateController() {
         var vm = this;
+        
+        window.bboxInit = function () {
+           bbox.showForm('5caf7b51-d57e-439e-be9b-4cd9da32ba10');
+       };
+
+        var e = document.createElement('script'); e.async = true;
+        e.src = 'https://bbox.blackbaudhosting.com/webforms/bbox-min.js';
+        document.getElementsByTagName('head')[0].appendChild(e);
     }
 })();
