18140bcd58dce74761df28683604c5032fe5dcc4
Justin Palmer <Justin.Palmer@walmart.com>
2016-11-20 22:17:07 -0600

    Finished spinner button component.

--- app/components/spinnerButton/spinnerButton.component.js
+++ app/components/spinnerButton/spinnerButton.component.js
@@ -7,23 +7,36 @@
                 clickFn: '&',
                 stopFn: '&'
             },
-            template: `<md-button class="md-raised button-primary" ng-click="vm.clickFunction()">{{::vm.text}}</md-button>`,
+            template: `<md-button class="spinner-button md-raised button-primary" ng-click="vm.clickFunction()" ng-disabled="vm.buttonDisabled">
+                            <span ng-show="!vm.spinnerShown">{{::vm.text}}</span>
+                            <md-progress-circular md-mode="indeterminate" md-diameter="20" ng-show="vm.spinnerShown"></md-progress-circular>
+                        </md-button>`,
             controller: controller,
             controllerAs: 'vm'
         });
 
-    controller.$inject = ['$timeout', '$window'];
+    controller.$inject = ['$element', '$attrs', '$window', '$timeout'];
 
-    function controller($timeout, $window) {
+    function controller($element, $attrs, $window, $timeout) {
         let vm = this;
+
+        vm.spinnerShown = false;
+        vm.buttonDisabled = false;
 
         vm.clickFunction = function() {
             let t0 = $window.performance.now();
+            let width = $element.prop('offsetWidth');
+            let otherCss = $element.css('cssText');
+
+            vm.buttonDisabled = true;
+            $attrs.$set('style', `width: ${width}px; ${otherCss}`);
 
             vm.clickFn().then(() => {
                 let time = $window.performance.now() - t0;
 
-                if(time > 50) {
+                if(time > 100) {
+                    vm.spinnerShown = true;
+
                     $timeout(() => {
                         vm.stopFn();
                     }, 1000);
