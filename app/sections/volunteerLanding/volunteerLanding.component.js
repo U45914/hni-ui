040e5f3f0467a32a0d6d5903874dadd581fca1dc
Justin Palmer <Justin.Palmer@walmart.com>
2016-12-01 12:30:46 -0600

    Styling updates to volunteer landing;

--- app/sections/volunteerLanding/volunteerLanding.component.js
+++ app/sections/volunteerLanding/volunteerLanding.component.js
@@ -8,7 +8,21 @@
             controllerAs: 'vm'
         });
 
-    function controller() {
+    controller.$inject = ['$window', '$state'];
 
+    function controller($window, $state) {
+        let vm = this;
+
+        vm.placeOrders = function() {
+            $state.go('order-detail');
+        };
+
+        vm.openVideo = function() {
+            $window.open('https://www.youtube.com/watch?v=aDuojsfsU2c', '_blank');
+        };
+
+        vm.signUp = function() {
+            $window.open('https://calendly.com/hunger/30-minute-volunteer-session-hunger-not-impossible/12-01-2016', '_blank');
+        };
     }
 })();
