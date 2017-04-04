6cfef4f0a523e2294ac37f91c8ed9e19d53b39f0
U47572 <U47572@C250LTRV.ustr.com>
2017-04-04 14:41:44 +0530

    Resolved conflict

--- app/sections/dashboard/actionsSection/actionsSection.component.js
+++ app/sections/dashboard/actionsSection/actionsSection.component.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 (function() {
     angular
         .module('app')
@@ -26,4 +27,35 @@
             });
         };
     }
+=======
+(function() {
+    angular
+        .module('app')
+        .component('actionsSection', {
+            bindings: {},
+            templateUrl: 'actions-section.tpl.html',
+            controller: ActionSectionController,
+            controllerAs: 'vm'
+        });
+
+    ActionSectionController.$inject = ['$http','userService'];
+
+    function ActionSectionController($http,userService) {
+        var vm = this;
+
+        vm.$onInit = function() {
+        	debugger;
+           // vm.userRole = 1;
+        	$http.get('http://localhost:8080/hni-admin-service/api/v1/users/services')
+            .then(function success(response) {
+                if(response.data !== null) {
+                   console.log("response : "+response.data);
+                   vm.user = response.data ;
+                }
+            }, function error(error) {
+                console.log(error);
+            });
+        };
+    }
+>>>>>>> Stashed changes
 })();
