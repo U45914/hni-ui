3f314cecfe35870ef208c1edddc7888bd0d6b99d
gakiz1 <ganeshvk93@gmail.com>
2017-04-24 20:37:55 +0530

    Integrated Report View UI with service

--- app/sections/dashboard/reportGeneration/reportGeneration.component.js
+++ app/sections/dashboard/reportGeneration/reportGeneration.component.js
@@ -24,23 +24,66 @@
 		let baseUrl = serviceConstants.baseUrl;
         var vm = this;
         
-        vm.service =     [{ "name": "Rahul" }];
-        vm.headers =     [{  "field": "name",  "label": "FirstName" }];
-        $scope.serviceds = vm.service;
-        $scope.headerds = vm.headers;
 
-        	/*let baseUrl = serviceConstants.baseUrl;
-        	$http.get(`${baseUrl}/view/`)
+        	//let baseUrl = serviceConstants.baseUrl;
+        	$http.get(`${baseUrl}/reports/view/customers/role`)
             .then(function success(response) {
                 if(response.data !== null) {
-                   console.log("response : "+response.data);
-                   vm.service = response.data.data;
-                   vm.headers= response.data.headers;
+                   console.log("response : ");
+                   console.log(response.data);
+                   vm.serviceRole = response.data.data;
+                   vm.headersRole= response.data.headers;
                 }
             }, function error(error) {
                 console.log(error);
-            });*/
-	
+            });
+        
+        	$http.get(`${baseUrl}/reports/view/customers/organization`)
+            .then(function success(response) {
+                if(response.data !== null) {
+                   console.log("response : ");
+                   console.log(response.data);
+                   vm.serviceOrganization = response.data.data;
+                   vm.headersOrganization= response.data.headers;
+                }
+            }, function error(error) {
+                console.log(error);
+            });
+        	$http.get(`${baseUrl}/reports/view/customers/ngo`)
+            .then(function success(response) {
+                if(response.data !== null) {
+                   console.log("response : ");
+                   console.log(response.data);
+                   vm.serviceNgo = response.data.data;
+                   vm.headersNgo= response.data.headers;
+                }
+            }, function error(error) {
+                console.log(error);
+            });
+        	$http.get(`${baseUrl}/reports/view/NGO`)
+            .then(function success(response) {
+                if(response.data !== null) {
+                   console.log("response : ");
+                   console.log(response.data);
+                   vm.serviceGetAllNGO = response.data.data;
+                   vm.headersGetAllNGO = response.data.headers;
+                }
+            }, function error(error) {
+                console.log(error);
+            });
+            
+        	
+        	$http.get(`${baseUrl}/reports/view/getAllVolunteers`)
+            .then(function success(response) {
+                if(response.data !== null) {
+                   console.log("responseAkkara : ");
+                   console.log(response.data);
+                   vm.serviceVolunteer = response.data.data;
+                   vm.headersVolunteer= response.data.headers;
+                }
+            }, function error(error) {
+                console.log(error);
+            });
 }
 	
 })();
