1443cb244a2fe5687c3bcca74cee231eeda49d6e
Rahul <Rahul.Nair@ust-global.com>
2017-04-27 20:19:29 +0530

    User address issues fixing

--- app/sections/clientsEnrollment/PersonalInfoTab.component.js
+++ app/sections/clientsEnrollment/PersonalInfoTab.component.js
@@ -1,65 +1,63 @@
-(function () {
-  angular
-      .module('app')
-      
-      .directive('personalInfoTab', personalInfoDirective)
-	
-	
-	function personalInfoDirective() {
-		return {
-			scope : {
-				
-			},
-			restrict : "E",
-			templateUrl : "personalInfoTab.tpl.html",
-			controller : personalInfoController,
-			controllerAs: 'vm'
-		}
+(function() {
+    angular
+        .module('app')
 
-	} 
-  
-  personalInfoController.$inject = ['$q','clientEnrollmentService','$rootScope']; 
-  
-  function personalInfoController ($q,clientEnrollmentService,$rootScope) {
-	  var vm = this;
-	  
-	  vm.save = function(){
-		  var data = {
-					"firstName" : vm.user.firstName,
-					"middleName" : vm.user.middleName,
-					"lastName" : vm.user.lastName,
-					"address" : {
-						"name" : vm.client.name,
-						"address1" : vm.client.address1,
-						"address2" : vm.client.address2,
-						"city" : vm.client.city,
-						"state" : vm.client.state,
-						"zip" : vm.client.zip,
-					},
-					"ethnicity" : vm.user.ethnicity,
-					"phoneNumber" : vm.user.phoneNumber,
-					"bday" : vm.client.bday,
-					"beenArrested" : vm.client.beenArrested,
-					"felony" : vm.user.felony
-					};
-		  		console.log("client personal Info : ");
-		  		console.log(data);
-				if (vm.user.ethnicity != null && vm.user.phoneNumber != null
-						&& vm.client.bday != null) {
-					var serviceCalls = clientEnrollmentService.setPersonnalData(data);
-					//var serviceCalls = clientEnrollmentService.savePartial();
-					$q.all(serviceCalls)// .then(onSuccess,onError);
-					$rootScope.$broadcast("scroll-tab", [ 1, 2 ]);
+        .directive('personalInfoTab', personalInfoDirective)
 
-				} else {
-					window.alert("Please fill Fields");
 
-					return false;
-				}
-	  }
-  
-//		  alert("entered to personnal Info Controller!!!");
-	  }
-  
- })();
-        
+    function personalInfoDirective() {
+        return {
+            scope: {
+
+            },
+            restrict: "E",
+            templateUrl: "personalInfoTab.tpl.html",
+            controller: personalInfoController,
+            controllerAs: 'vm'
+        }
+
+    }
+
+    personalInfoController.$inject = ['$q', 'clientEnrollmentService', '$rootScope'];
+
+    function personalInfoController($q, clientEnrollmentService, $rootScope) {
+        var vm = this;
+
+        vm.save = function() {
+            var data = {
+                "firstName": vm.user.firstName,
+                "middleName": vm.user.middleName,
+                "lastName": vm.user.lastName,
+                "address": {
+                    "name": vm.client.name,
+                    "address1": vm.client.address1,
+                    "address2": vm.client.address2,
+                    "city": vm.client.city,
+                    "state": vm.client.state,
+                    "zip": vm.client.zip,
+                },
+                "ethnicity": vm.user.ethnicity,
+                "phoneNumber": vm.user.phoneNumber,
+                "bday": vm.client.bday,
+                "beenArrested": vm.client.beenArrested,
+                "felony": vm.user.felony
+            };
+            console.log("client personal Info : ");
+            console.log(data);
+            if (vm.user.ethnicity != null && vm.user.phoneNumber != null &&
+                vm.client.bday != null) {
+                var serviceCalls = clientEnrollmentService.setPersonnalData(data);
+                //var serviceCalls = clientEnrollmentService.savePartial();
+                $q.all(serviceCalls) // .then(onSuccess,onError);
+                $rootScope.$broadcast("scroll-tab", [1, 2]);
+
+            } else {
+                window.alert("Please fill Fields");
+
+                return false;
+            }
+        }
+
+    }
+
+})();
