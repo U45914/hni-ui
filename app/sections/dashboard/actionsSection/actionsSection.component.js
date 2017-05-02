4640deddca99d90f7e0e4ec12a2ad38a00fee12c
gakiz1 <ganeshvk93@gmail.com>
2017-05-02 11:49:36 +0530

    Made workspace upto date with develop_ui

--- app/sections/dashboard/actionsSection/actionsSection.component.js
+++ app/sections/dashboard/actionsSection/actionsSection.component.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 (function() {
     angular
         .module('app')
@@ -50,50 +49,4 @@
         	
         };
     }
-=======
-(function() {
-    angular
-        .module('app')
-        .component('actionsSection', {
-            bindings: {},
-            templateUrl: 'actions-section.tpl.html',
-            controller: ActionSectionController,
-            controllerAs: 'vm'
-        });
-
-    ActionSectionController.$inject = ['$scope', '$http','userService', 'serviceConstants'];
-
-    function ActionSectionController($scope, $http,userService, serviceConstants) {
-    	let baseUrl = serviceConstants.baseUrl;
-        var vm = this;
-         
-   	 	vm.service = [];
-   	 	vm.headers= [];
-   	 	vm.reportCollection = [];
-        
-        vm.$onInit = function() {
-        	let baseUrl = serviceConstants.baseUrl;
-        	$http.get(`${baseUrl}/users/services`)
-            .then(function success(response) {
-            	
-                if(response.data !== null) {
-                   console.log("response : "+response.data);
-                   vm.user = response.data.data;
-                }
-            }, function error(error) {
-                console.log(error);
-            });
-        	
-        	$http.get(`${baseUrl}/users/reports`)
-            .then(function success(response) {
-                if(response.data !== null) {
-                   vm.reportCollection = response.data.data;
-                }
-            }, function error(error) {
-                console.log(error);
-            });
-        	
-        };
-    }
->>>>>>> Stashed changes
 })();
