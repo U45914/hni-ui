9a6692700c5904008304e2f83e878aafbbd64f2c
Rahul <Rahul.Nair@ust-global.com>
2017-05-04 10:56:30 +0530

    Styles changes from Josna integrated

--- app/sections/dashboard/dashboard.component.js
+++ app/sections/dashboard/dashboard.component.js
@@ -8,10 +8,10 @@
            
             <top-nav></top-nav>
             			<div layout="row" layout-align="space-around none">
-            				<md-content flex="25" layout-padding class="md-whiteframe-4dp">
+            				<md-content flex="20" layout-padding class="md-whiteframe-4dp">
 		            			<left-nav-bar></left-nav-bar>
 		            		</md-content>
-		            		<md-content flex="75" layout-padding class="md-whiteframe-4dp">
+		            		<md-content flex="80" layout-padding class="md-whiteframe-4dp">
 		                    	<actions-section></actions-section>
 		                    	<div>
 		                        	<user-list></user-list>
