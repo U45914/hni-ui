a478950284f171de1498bbfbd2e617d610883ebd
somdas <U47592@ust-global.com>
2017-05-12 17:19:46 +0530

    Made UI changes.

--- app/services/userService.service.js
+++ app/services/userService.service.js
@@ -12,6 +12,7 @@
         const FIRST_NAME = 'hni_firstName';
         const LAST_NAME = 'hni_lastName';
         const PHONE = 'hni_mobilePhone';
+        const ORGANIZATION_NAME = 'hni_organizationName';
 
         loadUserDetails();
 
@@ -28,7 +29,7 @@
 
         function setUser(data) {
             user = data;
-            storeUserDetails(user.id, user.email, user.firstName, user.lastName, user.mobilePhone);
+            storeUserDetails(user.id, user.email, user.firstName, user.lastName, user.mobilePhone, user.organizationName);
         }
 
         function loadUserDetails() {
@@ -37,14 +38,16 @@
             user.firstName = window.localStorage.getItem(FIRST_NAME);
             user.lastName = window.localStorage.getItem(LAST_NAME);
             user.mobilePhone = window.localStorage.getItem(PHONE);
+            user.organizationName =  window.localStorage.getItem(ORGANIZATION_NAME);
         }
 
-        function storeUserDetails(id, email, firstName, lastName, mobilePhone) {
+        function storeUserDetails(id, email, firstName, lastName, mobilePhone, organizationName) {
             window.localStorage.setItem(USER_ID, id);
             window.localStorage.setItem(USER_EMAIL, email);
             window.localStorage.setItem(FIRST_NAME, firstName);
             window.localStorage.setItem(LAST_NAME, lastName);
             window.localStorage.setItem(PHONE, mobilePhone);
+            window.localStorage.setItem(ORGANIZATION_NAME, organizationName);
         }
 
         function removeUserDetails() {
