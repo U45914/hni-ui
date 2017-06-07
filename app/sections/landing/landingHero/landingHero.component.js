4ec5fb1b4bbe3cf827fa737b1116b1454b4b3422
U47572 <U47572@ust-global.ustr.com>
2017-06-07 14:58:45 +0530

    Added "Number of Participants" and "Number of meals" to Home page

--- app/sections/landing/landingHero/landingHero.component.js
+++ app/sections/landing/landingHero/landingHero.component.js
@@ -8,17 +8,27 @@
             controllerAs: 'vm'
         });
 
-    LandingHeroController.$inject = ['hashScroll','$state'];
+    LandingHeroController.$inject = ['hashScroll','$state','$http','serviceConstants'];
 
-    function LandingHeroController(hashScroll,$state) {
+    function LandingHeroController(hashScroll,$state,$http,serviceConstants) {
+    	let baseUrl = serviceConstants.baseUrl;
         var vm = this;
-
+        vm.participants=0;
+        vm.meals =0;
         vm.scrollTo = function(hash) {
             hashScroll.scrollToHash(hash);
         };
-        
-        vm.load = function(){
-        	$state.go('login');
-        }
-    }
+               
+        vm.$onInit = function() {
+            $http.get(`${baseUrl}/reports/view/get/customers/all/orders`).then(function success(response) {
+            	if(response.data != null) {
+            	vm.participants = response.data.data.totalClient;
+            	vm.meals = response.data.data.totalOrders;
+            	}
+            } , function error(error) { 
+                console.log(error);
+            });
+            }
+       
+    }   
 })();
